5534cc351bcab1457fb81de8c0e644cfe1d15998
Shiju Varghese <jaspreet@Shijus-MacBook-Pro.local>
2018-07-15 12:00:16 +0530

    naviagtion bar gradient color

--- GraidentNavigationBar/ViewController.swift
+++ GraidentNavigationBar/ViewController.swift
@@ -12,14 +12,32 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+   
+    self.title = "Gradient Navigation Bar"
+    
+    if let navFrame = self.navigationController?.navigationBar.frame {
+      
+      //Create a new frame with the default offset of the status bar
+      let newframe = CGRect(origin: .zero, size: CGSize(width: navFrame.width, height: (navFrame.height + UIApplication.shared.statusBarFrame.height) ))
+      
+      let image = gradientWithFrametoImage(frame: newframe, colors: [UIColor.red.cgColor, UIColor.blue.cgColor])!
+      
+      self.navigationController?.navigationBar.barTintColor = UIColor(patternImage: image)
+      
+    }
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  
+  func gradientWithFrametoImage(frame: CGRect, colors: [CGColor]) -> UIImage? {
+    let gradient: CAGradientLayer  = CAGradientLayer(layer: self.view.layer)
+    gradient.frame = frame
+    gradient.colors = colors
+    UIGraphicsBeginImageContext(frame.size)
+    gradient.render(in: UIGraphicsGetCurrentContext()!)
+    let image = UIGraphicsGetImageFromCurrentImageContext()
+    UIGraphicsEndImageContext()
+    return image
   }
-
 
 }
 
